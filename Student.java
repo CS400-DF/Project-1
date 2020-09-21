e94d385663bdb3a830c4dd5e69e24f7f9edf55b9
nicgathman <ngathman@wisc.edu>
2020-09-21 16:54:42 -0500

    Added draft Student class

---
+++ Student.java
@@ -0,0 +1,31 @@
+
+public class Student {
+	int id;
+	String name;
+	String healthStatus;
+	String residence;
+	
+	public Student(int id, String name, String healthStatus, String residence){
+	  this.id = id;
+	  this.name = name;
+	  this.healthStatus = healthStatus;
+	  this.residence = residence;
+	}
+	
+	public int getID() {
+	  return this.id;
+	}
+	
+	public String getName() {
+	  return this.name;
+	}
+	
+	public String getHealthStatus() {
+	  return this.healthStatus;
+	}
+	
+	public String getResidence() {
+	  return this.residence;
+	}
+
+}
