d0169d7542519916cb524be9329d1eedbed2ded4
nicgathman <55770868+nicgathman@users.noreply.github.com>
2020-09-26 17:30:23 -0500

    Update CovidApp.java

--- CovidApp.java
+++ CovidApp.java
@@ -4,17 +4,21 @@ import java.io.*;
 
 public class CovidApp {
 
-	LinkedList<Student>[] hashTable;
+
 	@SuppressWarnings("unchecked")
 	int numElements = 0;
 	public CovidApp() {
-	  //initially 10?
-	  hashTable = new LinkedList[10];
+
 	  
 	  
 	  
 	}
-
+	/**
+	 * @author Nicole Gathman
+	 * Method loads student information into the hash table from a parsed text file
+	 * Skips students with incorrect student information
+	 * @return textFile is the string name of the information file given by user
+	 */
 	public void load(String textFile) {
 	  ArrayList<String> options = new ArrayList<>(); 
 	  options.add("positive");
@@ -22,40 +26,42 @@ public class CovidApp {
 	  options.add("results-pending");
 	  options.add("on-campus");
 	  options.add("off-campus");
-		try {
-	      Scanner scan = new Scanner(new File(textFile));
-	      while(scan.hasNextLine()) {
-	    	if(scan.nextLine().contains(",") == false) {
-	    	  System.out.println("Student information needs to be separated by commas"
-	    	      +  "\n" + "_______STUDENT IS SKIPPED_____");
-	    	}
-	    	else {	
-	          String[] data = scan.nextLine().split(",");
-	          if(data.length != 4) {
-		        System.out.println("Student information is invalid"
-			        +  "\n" + "_______STUDENT IS SKIPPED_____");
-	          }
-	          // indexes....0 = ID, 1 = Name, 2 = status, 3 = residence
-	          else if(containsKey(Integer.parseInt(data[0])) == true){
-	            System.out.println("Student with ID: " + data[0] + " already exists" + "/n "
-	                + "_______STUDENT IS SKIPPED_____");
-	          }
-	          else if(data[0].length() != 9){
-		        System.out.println("Student with ID: " + data[0] + " is invalid" + "\n "
-		        	+ "_______STUDENT IS SKIPPED_____");
-	          }
-	          else if(options.contains(data[2].toLowerCase()) == false || 
-	        		options.contains(data[3].toLowerCase()) == false) {
-	            System.out.println("Information entered is invalid" + "\n" 
-	        		+ "_______STUDENT IS SKIPPED_____");
-	          }
-	          else {
-	        	System.out.println("add");
-	            addStudent(Integer.parseInt(data[0]), data[1], data[2], data[3]);
-	          } 
+	  int fileLine = 0;
+	  try {
+	    Scanner scan = new Scanner(new File(textFile));
+	    while(scan.hasNextLine()) {
+	      fileLine++;
+	      String currentLine = scan.nextLine();
+	      if(currentLine.contains(",") == false) {
+	        System.out.println("Student information needs to be separated by commas"
+	    	    + "at line number " + fileLine+ "\n" + "_______STUDENT IS SKIPPED_____");
+	      }
+	      else {	
+	        String[] data = currentLine.split(",");
+	        if(data.length != 4) {
+		  System.out.println("Student information is invalid"
+	              + " at line number " + fileLine + "\n" + "_______STUDENT IS SKIPPED_____");
 	        }
-		  }
-	      scan.close(); 
+	        // indexes....0 = ID, 1 = Name, 2 = status, 3 = residence
+	        else if(containsKey(Integer.parseInt(data[0])) == true){
+	          System.out.println("Student with ID: " + data[0] + " already exists" + "/n "
+	              + "_______STUDENT IS SKIPPED_____");
+	        }
+	        else if(data[0].length() != 9){
+		      System.out.println("Student with ID: " + data[0] + " at line " +fileLine +
+		          " is invalid" + "\n " + "_______STUDENT IS SKIPPED_____");
+	        }
+	        else if(options.indexOf(data[2].toLowerCase()) == -1 || 
+	            options.indexOf(data[3].toLowerCase()) == -1) {
+	          System.out.println("Information entered is invalid at line number " + fileLine
+	              + "\n" + "_______STUDENT IS SKIPPED_____");
+	        }
+	        else {
+	          addStudent(Integer.parseInt(data[0]), data[1], data[2], data[3]);
+	        } 
+	      }
+            }
+	    scan.close(); 
 	  }
 	  catch(NoSuchElementException e) {
 	    e.getMessage();
@@ -63,16 +69,12 @@ public class CovidApp {
 	  catch(IllegalStateException e) {
 	    e.getMessage();  
 	  }
-      catch(FileNotFoundException e) {
+          catch(FileNotFoundException e) {
 		e.getMessage();
 	  }
     }
-	//other team members...
-	
-	public boolean put(Student student) {
-	    int id = student.id; // this is the key
 
-	}
+
 	
 	
 	
