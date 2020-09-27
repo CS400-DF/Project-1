fac22cd3b963da61482d3e09e6a7dcf45e188fbc
aaaabhaypunjabiiii <56175235+aaaabhaypunjabiiii@users.noreply.github.com>
2020-09-28 01:57:42 +0400

    Implemented methods to suit the Driver class

--- Student.java
+++ Student.java
@@ -1,52 +1,64 @@
 /**
- * 
- */
-
-/**
  * @author abhay
  *
  */
 public class Student {
-	private int studentId;
+	private Integer studentId;
 	private String name;
 	private String healthStatus;
 	private String residenceInfo;
-	
 
-	public Student(int studentId, String name, String healthStatus, String residenceInfo) {
+	public Student() {
+
+	}
+	public Student(Integer studentId, String name, String healthStatus, String residenceInfo) {
 		this.studentId = studentId;
 		this.name = name;
 		this.healthStatus = healthStatus;
 		this.residenceInfo = residenceInfo;
 	}
+
 	/**
 	 * @return the studentId
 	 */
-	public int getStudentId() {
+	public Integer getStudentId() {
 		return studentId;
 	}
-	
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	/**
 	 * @return the healthStatus
 	 */
 	public String getHealthStatus() {
-		return healthStatus;
+		return this.healthStatus;
 	}
 
 	/**
 	 * @return the residenceInfo
 	 */
 	public String getResidenceInfo() {
-		return residenceInfo;
+		return this.residenceInfo;
 	}
 
+	/**
+	 * Student details are printed
+	 */
+	public void printStudent() {
+		System.out.println(
+				"Name: " + this.name + " \nHealth status: " + this.healthStatus + "\nResidence: " + this.residenceInfo);
+	}
 	
-	
+	/**
+	 * @return String containing student details
+	 */
+	public String getValue() {
+		return (String)(getName()+" "+getHealthStatus()+" "+getResidenceInfo()+" ");
+	}
+
 }
