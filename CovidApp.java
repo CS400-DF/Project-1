f1a7cf3603d159c984b2ac4a1497f9826aa63199
aaaabhaypunjabiiii <56175235+aaaabhaypunjabiiii@users.noreply.github.com>
2020-09-28 02:41:53 +0400

    Final commit
    Contains my implementation of the HashTableMap and the load() method

--- CovidApp.java
+++ CovidApp.java
@@ -1,87 +1,170 @@
-//hash table class 
-import java.util.*;
+/**
+ * @author abhay
+ *
+ */
 import java.io.*;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 
 public class CovidApp {
 
+	public HashTableMap<Integer, Student> map;
+	public String[] healthStatus = new String[] { "positive", "negative", "results-pending" };
+	public String[] location = new String[] { "off-campus", "on-campus" };
 
-	@SuppressWarnings("unchecked")
-	int numElements = 0;
 	public CovidApp() {
-
-	  
-	  
-	  
+		this.map = new HashTableMap();
 	}
+
 	/**
-	 * @author Nicole Gathman
-	 * Method loads student information into the hash table from a parsed text file
-	 * Skips students with incorrect student information
-	 * @return textFile is the string name of the information file given by user
+	 * This method add a student's information into the map
+	 * 
+	 * @return true if the student is successfully added, false otherwise
 	 */
-	public boolean load(String textFile) {
-          boolean fileCorrect = true;
-	  ArrayList<String> options = new ArrayList<>(); 
-	  options.add("positive");
-	  options.add("negative");
-	  options.add("results-pending");
-	  options.add("on-campus");
-	  options.add("off-campus");
-	  int fileLine = 0;
-	  try {
-	    Scanner scan = new Scanner(new File(textFile));
-	    while(scan.hasNextLine()) {
-	      fileLine++;
-	      String currentLine = scan.nextLine();
-	      if(currentLine.contains(",") == false) {
-	        System.out.println("Student information needs to be separated by commas"
-	    	    + "at line number " + fileLine+ "\n" + "_______STUDENT IS SKIPPED_____");
-	      }
-	      else {	
-	        String[] data = currentLine.split(",");
-	        if(data.length != 4) {
-		  System.out.println("Student information is invalid"
-	              + " at line number " + fileLine + "\n" + "_______STUDENT IS SKIPPED_____");
-	        }
-	        // indexes....0 = ID, 1 = Name, 2 = status, 3 = residence
-	        else if(containsKey(Integer.parseInt(data[0])) == true){
-	          System.out.println("Student with ID: " + data[0] + " already exists" + "/n "
-	              + "_______STUDENT IS SKIPPED_____");
-	        }
-	        else if(data[0].length() != 9){
-		      System.out.println("Student with ID: " + data[0] + " at line " +fileLine +
-		          " is invalid" + "\n " + "_______STUDENT IS SKIPPED_____");
-	        }
-	        else if(options.indexOf(data[2].toLowerCase()) == -1 || 
-	            options.indexOf(data[3].toLowerCase()) == -1) {
-	          System.out.println("Information entered is invalid at line number " + fileLine
-	              + "\n" + "_______STUDENT IS SKIPPED_____");
-	        }
-	        else {
-	          addStudent(Integer.parseInt(data[0]), data[1], data[2], data[3]);
-	        } 
-	      }
-            }
-	    scan.close(); 
-	  }
-	  catch(NoSuchElementException e) {
-	    e.getMessage();
-	    fileCorrect = false;
-	  }
-	  catch(IllegalStateException e) {
-	    e.getMessage(); 
-            fileCorrect = false;
-	  }
-          catch(FileNotFoundException e) {
-	    e.getMessage();
-	    fileCorrect = false;
-	  }
-	  finally{
-            return fileCorrect;
-         }
+	public boolean addStudent(Integer id, String name, String healthStatus, String housingStatus) {
+		Student student = new Student(id, name, healthStatus, housingStatus);
+		return map.put(id, student);
+	}
 
+	/**
+	 * This method returns a student object based on the given ID number
+	 * 
+	 * @param key - student ID number
+	 * @return student object
+	 * @throws NoSuchElementException
+	 */
+	public Student getStudentInformation(Integer id) throws NoSuchElementException {
+		if (!(map.containsKey(id))) {
+			throw new NoSuchElementException();
+		} else {
+			return (Student) map.get(id);
+		}
 
-	
-	
-	
+	}
+
+	/**
+	 * This methods gets the number of student objects stored in the map
+	 * 
+	 * @return the number of student objects stored in the map
+	 */
+	public int getAmountOfStudents() {
+		return map.size();
+
+	}
+
+	/**
+	 * This method checks if a student already has information in the map
+	 * 
+	 * @param key - the student ID number
+	 * @return true if the student is in the map, false otherwise
+	 */
+	public boolean containsStudent(int id) {
+		return map.containsKey(id);
+
+	}
+
+	/**
+	 * This method removes a student from the map given the ID number
+	 * 
+	 * @param key - student ID number
+	 * @return the Student object being removed
+	 */
+	public Student removeStudent(int id) {
+		return (Student) map.remove(id);
+	}
+
+	/**
+	 * This method clears all the students in the map
+	 */
+	public void clearStudents() {
+		map.clear();
+	}
+
+	/**
+	 * This method is responsible for parsing a text-file, each line is checked for
+	 * its validity. Valid lines are parsed and stored in the hashtable.
+	 * 
+	 * @param FileName
+	 * @return true or false depending on whether the file was parsed correctly
+	 */
+	public boolean load(String FileName) {
+		boolean fileFound = true;
+		File data;
+		Scanner scnr;
+		int lineCount = 0;
+		try {
+			// checks if file exists
+			
+			data = new File(FileName);
+			
+			scnr = new Scanner(data);
+			while (scnr.hasNextLine()) {
+				
+				// file is parsed
+				lineCount++;
+				// lines are counted to indicate errors to the user
+				// each line is split
+				String[] details = scnr.nextLine().split(",");
+				if (details.length != 4) {
+					// if the string array does not contain 4 elements
+					System.out.println("Invalid line, line " + lineCount + " is skipped, with key " + details[0]);
+					// moves on to the next line
+					continue;
+				} else {
+					// line validity checked
+					if (checkLineValid(details)) {
+							// student is added if line is valid
+							addStudent((Integer.parseInt(details[0])), details[1], details[2], details[3]);
+						
+					} else {
+
+						System.out.println("The health status and/or location have not been entered correctly on line "
+								+ lineCount + " with key " + details[0] + " is skipped");
+
+					}
+				}
+			}
+			scnr.close();
+		} catch (FileNotFoundException excpt) {
+			// FNFE exception
+			fileFound = false;
+			excpt.getStackTrace();
+		}
+		// returns true/false if file was parsed successfully
+		return fileFound;
+	}
+
+	/**
+	 * A string array containing the details parsed from a single line is checked
+	 * 
+	 * @param data
+	 * @return true/false if line is valid
+	 */
+	public boolean checkLineValid(String[] data) {
+		boolean checkStatus = false, checkLocation = false, checkIdSize = false;
+		
+		for (int i = 0; i < healthStatus.length; i++) {
+			//checks if the status falls under one of the 3 possible statuses
+			if (data[2].equalsIgnoreCase(healthStatus[i])) {
+				checkStatus = true;
+				break;
+			}
+		}
+		for (int j = 0; j < location.length; j++) {
+			//checks if the status falls under one of the 2 possible residential statuses
+			if (data[3].equalsIgnoreCase(location[j])) {				
+				checkLocation = true;
+				break;
+			}
+		}
+		//length of the id must be 9
+		if (data[0].length() == 9) {			
+			checkIdSize = true;
+		}
+		//System.out.print(checkIdSize && checkStatus && checkLocation);
+		return checkStatus && checkLocation && checkIdSize;
+	}
+
 }
--- Student.java
+++ Student.java
@@ -1,36 +1,64 @@
-
+/**
+ * @author abhay
+ *
+ */
 public class Student {
-	int id;
-	String name;
-	String healthStatus;
-	String residence;
-	
-	public Student(int id, String name, String healthStatus, String residence){
-	  this.id = id;
-	  this.name = name;
-	  this.healthStatus = healthStatus;
-	  this.residence = residence;
+	private Integer studentId;
+	private String name;
+	private String healthStatus;
+	private String residenceInfo;
+
+	public Student() {
+
 	}
-	
-	public int getID() {
-	  return this.id;
+	public Student(Integer studentId, String name, String healthStatus, String residenceInfo) {
+		this.studentId = studentId;
+		this.name = name;
+		this.healthStatus = healthStatus;
+		this.residenceInfo = residenceInfo;
 	}
-	
+
+	/**
+	 * @return the studentId
+	 */
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	/**
+	 * @return the name
+	 */
 	public String getName() {
-	  return this.name;
+		return this.name;
 	}
-	
+
+	/**
+	 * @return the healthStatus
+	 */
 	public String getHealthStatus() {
-	  return this.healthStatus;
+		return this.healthStatus;
+	}
+
+	/**
+	 * @return the residenceInfo
+	 */
+	public String getResidenceInfo() {
+		return this.residenceInfo;
+	}
+
+	/**
+	 * Student details are printed
+	 */
+	public void printStudent() {
+		System.out.println(
+				"Name: " + this.name + " \nHealth status: " + this.healthStatus + "\nResidence: " + this.residenceInfo);
 	}
 	
-	public String getResidence() {
-	  return this.residence;
+	/**
+	 * @return String containing student details
+	 */
+	public String getValue() {
+		return (String)(getName()+" "+getHealthStatus()+" "+getResidenceInfo()+" ");
 	}
-   
-	public void printStudent() {
-	  System.out.println("Name: " + this.name + " \nHealth status: " + this.healthStatus + "\nResidence: " + 
-			  this.residence);
-			  
-	}
+
 }
